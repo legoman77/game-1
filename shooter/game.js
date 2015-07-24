853212ed2bd311924b317d40b50c1c077800f05c
Rees Payne <rplegos@gmail.com>
2015-07-24 09:14:44 -0400

    save

---
+++ shooter/game.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
+  {preload:preload,create:create,update:update,render:render});
+
+function preload() {
+  game.stage.backgroundColor = '#bbbbbb';
+}
+
+function create() {
+}
+
+function update() {
+}
+
+function render() {
+}
